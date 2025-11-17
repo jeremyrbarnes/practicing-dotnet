50b6a24f89f176573d4fbd71ff00c48076d6b70c
Jeremy Barnes <jeremy.r.barnes@gmail.com>
2025-11-17 17:41:47 -0500

    adding coupon api

---
+++ Mango/Services/CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Mango.CouponAPI.Models;
+
+namespace Mango.Services.CouponAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+    }
+}

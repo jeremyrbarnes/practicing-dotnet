e1fcd9f45ab886dc6bfb0d01464b311bf3e7873f
jb <jb@jbs-Mac-Studio.local>
2025-12-18 13:30:57 -0500

    slight assembly info changes

--- ConferenceAttendees/ConferenceAttendees.Api/obj/Debug/net10.0/ConferenceAttendees.Api.AssemblyInfo.cs
+++ ConferenceAttendees/ConferenceAttendees.Api/obj/Debug/net10.0/ConferenceAttendees.Api.AssemblyInfo.cs
@@ -13,7 +13,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConferenceAttendees.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3842985b11ba1f25abd00424db1decf656c2456")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+698ebf04388c474379b16e89fbdd335690c23a99")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("ConferenceAttendees.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConferenceAttendees.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
